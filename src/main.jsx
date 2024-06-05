372b3b70c9aaa122b821325dd16d05fd3c3b348b
unknown <youngscientist.iysa@gmail.com>
2024-06-05 14:03:51 +0700

    fiveth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Home from "./pages";
 import Faq from "./pages/faq";
 
